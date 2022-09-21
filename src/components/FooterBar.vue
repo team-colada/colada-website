038bc51bad7971bab7967b31ff731cfd4011b240
Jonathan Chen <jonathan.hp.chen@gmail.com>
2022-09-21 10:56:23 -0700

    Added hamburger menu to header, made header and footer responsive

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="footer-bar flex justify-between items-center bg-slate-800">
-        <div class="footer-left flex justify-center items-center gap-4 pl-8">
+        <div class="footer-left flex justify-center items-center gap-3 pl-4">
             <a href="#" class="animate-bounce text-xl text-emerald-500 p-4 hover:text-emerald-400">
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-chevron-double-up" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M7.646 2.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 3.707 2.354 9.354a.5.5 0 1 1-.708-.708l6-6z"/>
@@ -11,7 +11,7 @@
             <a href="#install" class="text-slate-500 hover:text-slate-400">Installation</a>
             <a href="#contributors" class="text-slate-500 hover:text-slate-400">Contributors</a>
         </div>
-        <div class="footer-right flex justify-start items-center pr-8 gap-4">
+        <div class="footer-right flex justify-start items-center pr-4 gap-2">
             <h1>License MIT</h1>
             <a href="https://github.com/oslabs-beta/colada" target="_blank">
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,13 +1,13 @@
 <template>
     <nav class="header-bar fixed w-full flex justify-between items-center bg-slate-800 z-10">
-        <div class="header-left flex justify-evenly items-center pl-8 gap-4">
+        <div class="header-left  md:flex md:justify-evenly md:items-center md:w-auto hidden pl-8 gap-4">
             <img src="../assets/png/colada-logo.png" height="64" width="64" class="logo" alt="Colada logo" />
             <a href="#" class="text-xl font-bold text-orange-400 hover:text-orange-300">Colada</a>
             <a href="#features" class="text-lg text-slate-300 hover:text-slate-50">Features</a>
             <a href="#install" class="text-lg text-slate-300 hover:text-slate-50">Installation</a>
             <a href="#contributors" class="text-lg text-slate-300 hover:text-slate-50">Contributors</a>
         </div>
-        <div class="header-right flex justify-end items-center pr-8 gap-4">
+        <div class="header-right md:flex md:justify-end md:items-center md:w-auto hidden pr-8 gap-4">
             <a href="https://www.npmjs.com/package/colada-plugin" target="_blank">
                 <span class="rounded-lg border-2 border-slate-800 transition hover:border-orange-500 hover:text-orange-500 p-1 ">npm</span>
             </a>
@@ -15,24 +15,45 @@
                 <span class="rounded-lg text-slate-200 border-2 border-blue-700 bg-blue-700 transition hover:bg-slate-800 hover:text-blue-500 p-1 ">Chrome Extension</span>
             </a>
         </div>
-        <!-- <div class="flex justify-between items-center md:hidden">
-            <div class="flex justify-center items-center">
+        <div id="hamburger-icon" class="flex justify-between items-center w-full md:hidden">
+            <div class="flex justify-center items-center pl-4">
                 <img src="../assets/png/colada-logo.png" height="64" width="64" class="logo" alt="Colada logo" />
                 <a href="#" class="text-xl font-bold text-orange-400 hover:text-orange-300">Colada</a>
             </div>
-            <div class="space-y-2">
-                <span class="block w-8 h-0.5 bg-gray-600 animate-pulse"></span>
-                <span class="block w-8 h-0.5 bg-gray-600 animate-pulse"></span>
-                <span class="block w-8 h-0.5 bg-gray-600 animate-pulse"></span>
+            <div class="p-1 mr-3 border border-slate-800 rounded transition hover:border-slate-400 hover:cursor-pointer active:border-slate-200">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                </svg>
             </div>
-        </div> -->
+        </div>
+        <div id="hamburger-menu" class="hidden flex flex-col justify-around items-start gap-2 p-2 transition bg-slate-800 border border-slate-400 rounded absolute right-4 top-12">
+            <a href="#features" class="text-md w-full text-slate-300 hover:cursor-pointer hover:text-slate-50">Features</a>
+            <a href="#install" class="text-md w-full text-slate-300 hover:cursor-pointer hover:text-slate-50">Installation</a>
+            <a href="#contributors" class="text-md w-full text-slate-300 hover:cursor-pointer border-b border-slate-400 hover:text-slate-50">Contributors</a>
+            <a href="https://www.npmjs.com/package/colada-plugin" target="_blank">
+                <span class="text-md transition hover:cursor-pointer hover:text-orange-500">npm</span>
+            </a>
+            <a href="https://google.com/" target="_blank">
+                <span class="text-md text-blue-500 font-semibold transition hover:cursor-pointer hover:text-blue-400">Chrome Extension</span>
+            </a>
+        </div>
     </nav>
 </template>
 
 <script>
 
     export default {
-        name:'HeaderBar'
+        name:'HeaderBar',
+        mounted(){
+            const hamburgerIcon = document.getElementById('hamburger-icon')
+            hamburgerIcon.addEventListener('click', this.toggleMenu)
+        },
+        methods:{
+            toggleMenu(){
+                const hamburgerMenu = document.getElementById('hamburger-menu')
+                hamburgerMenu.classList.toggle('hidden')
+            }
+        }
     }
 </script>
 
